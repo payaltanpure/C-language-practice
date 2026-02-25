51b4b0b7d7af0d265fd2efcfb33c2760580ff672
unknown <tanpurepayal91@gmail.com>
2026-02-25 19:27:37 +0530

    donr

---
+++ functions/function2.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+void circlearea();
+void triarea();
+void recarea();
+int main()
+{  
+   menu1:
+   {
+   int ch,ct;
+   printf("\n1.Area of Circle \n2.Area of triangle \n 3.Area of rectangle\n 4. Exit");
+   printf("\nEnter the choice:");
+   scanf("%d", &ch);
+   switch(ch)
+   {
+      case 1:
+      circlearea();
+      break;
+
+      case 2:
+      triarea();
+      break;
+
+      case 3:
+      recarea();
+      break;
+
+      case 4:
+      printf("\nThanku");
+      return 0;
+      break;
+
+      default: 
+      printf("\nInvalid choice");
+
+   }
+   printf("\nENter 1 to conyinue and 0 to exit:");
+   scanf("%d", &ct);
+   if(ct==1)
+   {
+      goto menu1;
+   }
+   else{
+         printf("\nThanku");
+        return 0;
+   }
+}
+   return 0;
+}
+
+void circlearea()
+{
+    float  radius;
+    printf("\nEnter radius of circle:");
+    scanf("%f", &radius);
+    printf("\nArea of circle is :%f", (3.14 * radius * radius));
+
+}
+
+void triarea()
+{
+    float base , height;
+    printf("\nEnter the base and height:");
+    scanf("%f%f", &base , &height);
+    printf("\nArea of triangle is :%f", (0.5* base*height));
+}
+void recarea()
+{
+    float base , height;
+    printf("\nEnter the base and height:");
+    scanf("%f%f", &base , &height);
+    printf("\nArea of rectangle is :%f", (base*height));
+}
+
+
